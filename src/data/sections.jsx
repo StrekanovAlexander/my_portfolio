aecd25f2bdb8c9659ab0797008da6520ec748cda
Alexander Strekanov <strekanov.alexander@gmail.com>
2025-09-26 11:22:09 +0200

    added: experience functional

--- src/data/sections.jsx
+++ src/data/sections.jsx
@@ -54,18 +54,93 @@ export const sections = {
         </>,
       ],
     },
+    experience: {
+      title: "BERUFLICHE ERFAHRUNGEN",
+      icon: Briefcase,
+      left: [
+        <>
+          <h3 className="font-roboto font-extrabold mb-1">
+            FREIBERUFLICHE TÄTIGKEIT ALS PROGRAMMIERER
+          </h3>
+          <span className="text-gray-500">
+            Bad Kissingen 09.2025 – heute
+          </span>
+          <p className="text-gray-700 text-sm py-2">
+            Derzeit arbeite ich an dem Projekt Finping. Es handelt sich um einen SaaS-Service zur Beobachtung von Devisen- und Rohstoffmärkten. Dabei verwende ich Node.js, Sequelize, Express.js, React und Tailwind CSS. Als Market Data APIs arbeite
+            ich mit Yahoo Finance, CoinGecko und FXRates. 
+            <span className="block my-1"><a className="text-red-700" href="https://www.finping.space">https://www.finping.space</a></span>
+            <span className="block my-1">https://github.com/StrekanovAlexander/saas-finping</span>
+          </p>
+        </>,
+        <>
+          <h3 className="font-roboto font-extrabold mb-1">
+            SOFTWARE-INGENIEUR UND SYSTEMADMINISTRATOR
+          </h3>
+          <span className="text-gray-500">
+            Fa. PRAT ZZRK Mala Biloserka, Ukraine 01.2015 – 02.2022
+          </span>
+          <ul className="list-disc pl-6 space-y-2 text-gray-700 dark:text-gray-300 my-1">
+            <li>Entwicklung und Betreuung einer Anwendung zur Arbeitszeiterfassung (Delphi, Firebird Database)</li>
+            <li>Pflege der Unternehmens-Website (HTML, CSS, Java Script, PHP)</li>
+            <li>Zusätzliche Tätigkeit als Systemadministrator ab 02.2017</li>
+          </ul>
+        </>,
+      ],
+      right: [
+        <>
+          <h3 className="font-roboto font-extrabold mb-1">
+            SOFTWARENTWICKLER
+          </h3>
+          <span className="text-gray-500">
+            Malenkij Paris GmbH Moskau, Russland 10.2011 – 12.2014
+          </span>
+          <ul className="list-disc pl-6 space-y-2 text-gray-700 dark:text-gray-300 my-1">
+            <li>Entwicklung einer Online-Shop-Anwendung (PHP, HTML, CSS, Java Script)</li>
+          </ul>
+        </>,
+        <>
+          <h3 className="font-roboto font-extrabold mb-1">
+            SYSTEMADMINISTRATOR
+          </h3>
+          <span className="text-gray-500">
+            Kommunales Eigenunternehmen „Nasch Dim“ der Stadt Dniprorudne, Ukraine 12.2007 – 07.2011
+          </span>
+          <ul className="list-disc pl-6 space-y-2 text-gray-700 dark:text-gray-300 my-1">
+            <li>Pflege der lokalen Netzwerke (FoxPro)</li>
+            <li>Zuständig für ca. 50 Computer-Arbeitsplätze</li>
+            <li>Teilzeitbeschäftigung</li>
+          </ul>
+        </>,
+        <>
+          <h3 className="font-roboto font-extrabold mb-1">
+            SOFTWARENTWICKLER
+          </h3>
+          <span className="text-gray-500">
+            Fa. Arkob Dniprorudne, Ukraine 11.2006 – 10.2009
+          </span>
+          <ul className="list-disc pl-6 space-y-2 text-gray-700 dark:text-gray-300 my-1">
+            <li>
+              Entwicklung und Pflege von Anwendungen zur Stromverbrauchberechnung im Architekturbereich (Delphi, Firebird Database)
+            </li>
+          </ul>
+        </>,
+      ],
+    },
     skills: {
       title: "Fähigkeiten",
       icon: Laptop,
-      left: ["Lorem ipsum dolor sit amet...", "Curabitur ac augue..."],
+      left: [
+        
+        <>
+         
+        </>, 
+        
+        
+        "Curabitur ac augue..."
+      ],
       right: ["Praesent suscipit diam...", "Morbi feugiat ligula..."],
     },
-    experience: {
-      title: "Berufserfahrung",
-      icon: Briefcase,
-      left: ["Lorem ipsum dolor sit amet...", "Donec dapibus odio..."],
-      right: ["Pellentesque habitant...", "Vestibulum non velit..."],
-    },
+    
     projects: {
       title: "Persönliche Projekte",
       icon: Rocket,
@@ -80,18 +155,19 @@ export const sections = {
       left: ["Lorem ipsum dolor sit amet...", "Fusce non massa in tortor..."],
       right: ["Nullam dignissim...", "Sed commodo lorem..."],
     },
-    skills: {
-      title: "Навички",
-      icon: Laptop,
-      left: ["Lorem ipsum dolor sit amet...", "Curabitur ac augue..."],
-      right: ["Praesent suscipit diam...", "Morbi feugiat ligula..."],
-    },
     experience: {
       title: "Досвід роботи",
       icon: Briefcase,
       left: ["Lorem ipsum dolor sit amet...", "Donec dapibus odio..."],
       right: ["Pellentesque habitant...", "Vestibulum non velit..."],
     },
+    skills: {
+      title: "Навички",
+      icon: Laptop,
+      left: ["Lorem ipsum dolor sit amet...", "Curabitur ac augue..."],
+      right: ["Praesent suscipit diam...", "Morbi feugiat ligula..."],
+    },
+    
     projects: {
       title: "Особисті проєкти",
       icon: Rocket,
@@ -106,18 +182,19 @@ export const sections = {
       left: ["Lorem ipsum dolor sit amet...", "Fusce non massa in tortor..."],
       right: ["Nullam dignissim...", "Sed commodo lorem..."],
     },
-    skills: {
-      title: "Skills",
-      icon: Laptop,
-      left: ["Lorem ipsum dolor sit amet...", "Curabitur ac augue..."],
-      right: ["Praesent suscipit diam...", "Morbi feugiat ligula..."],
-    },
     experience: {
       title: "Experience",
       icon: Briefcase,
       left: ["Lorem ipsum dolor sit amet...", "Donec dapibus odio..."],
       right: ["Pellentesque habitant...", "Vestibulum non velit..."],
     },
+    skills: {
+      title: "Skills",
+      icon: Laptop,
+      left: ["Lorem ipsum dolor sit amet...", "Curabitur ac augue..."],
+      right: ["Praesent suscipit diam...", "Morbi feugiat ligula..."],
+    },
+    
     projects: {
       title: "Personal Projects",
       icon: Rocket,
